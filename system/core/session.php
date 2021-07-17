ca1dac6d14c3ed993ca50e3a938ebd7ae09e9953
Yaya Laressa <hallo@yaya.my.id>
2021-07-17 20:02:56 +0700

    Update session.php

--- system/core/session.php
+++ system/core/session.php
@@ -2,50 +2,52 @@
 
 class Session
 {
-	
-    function __construct()
-    {
-        $samesite = 'strict';
-        if(PHP_VERSION_ID < 70300) {
-            session_set_cookie_params('samesite='.$samesite);	
-        } else {
-            session_set_cookie_params(['samesite' => $samesite]);
-        }
-        if (isset($_COOKIE['PHPSESSID']))
-            session_start();
-    }
 
-    public function userdata($data)
-    {
-        if (session_status() == PHP_SESSION_NONE) return false;
-        if (isset($_SESSION[$data]))
-        {
-            return $_SESSION[$data];
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    public function set_userdata($data, $value = '')
-    {
-        if (session_status() == PHP_SESSION_NONE) session_start();
-        if (is_array($data))
+	function __construct()
+	{
+		$samesite = 'strict';
+		if (PHP_VERSION_ID < 70300)
 		{
-			foreach ($data as $key => &$value)
+			session_set_cookie_params('samesite=' . $samesite);
+		}
+		else
+		{
+			session_set_cookie_params(['samesite' => $samesite]);
+		}
+		if (isset($_COOKIE['PHPSESSID'])) session_start();
+	}
+
+	public function userdata($data)
+	{
+		if (session_status() == PHP_SESSION_NONE) return false;
+		if (isset($_SESSION[$data]))
+		{
+			return $_SESSION[$data];
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public function set_userdata($data, $value = '')
+	{
+		if (session_status() == PHP_SESSION_NONE) session_start();
+		if (is_array($data))
+		{
+			foreach ($data as $key => & $value)
 			{
 				$_SESSION[$key] = $value;
 			}
 
 			return;
 		}
-        $_SESSION[$data] = $value;
-    }
+		$_SESSION[$data] = $value;
+	}
 
-    public function unset_userdata($key)
-    {
-        if (is_array($key))
+	public function unset_userdata($key)
+	{
+		if (is_array($key))
 		{
 			foreach ($key as $k)
 			{
@@ -56,16 +58,17 @@ class Session
 		}
 
 		unset($_SESSION[$key]);
-    }
-    
-    public function has_userdata($key)
+	}
+
+	public function has_userdata($key)
 	{
 		return isset($_SESSION[$key]);
 	}
-	
+
 	public function sess_destroy()
 	{
 		session_destroy();
 	}
 
 }
+
